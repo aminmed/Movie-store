485290c37a70dfa6feb0a1c6acb5cb5528f7c940
Djameleddine21 <hd_sebbagh@esi.dz>
2021-05-23 00:22:18 +0100

    add search screens and controllers

---
+++ src/screens/SearchArticleForLocationController.java
@@ -0,0 +1,18 @@
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class SearchArticleForLocationController {
+
+    @FXML
+    private TextField idSearchArticle;
+
+    @FXML
+    private JFXButton idSearchArticleButton;
+
+    @FXML
+    void searchArticleForLocation(ActionEvent event) {
+
+    }
+
+}
---
+++ src/screens/SearchFilmByActorController.java
@@ -0,0 +1,18 @@
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class SearchFilmsByActorController {
+
+    @FXML
+    private TextField searchByActorField;
+
+    @FXML
+    private JFXButton idSearchFilmButton;
+
+    @FXML
+    void searchFilmsByActor(ActionEvent event) {
+
+    }
+
+}
